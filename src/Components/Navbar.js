19725e6edaeef231f5573c83654483f6758ca419
Aman <77211729+amanisreal@users.noreply.github.com>
2022-06-12 17:50:33 +0530

    Update Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,6 +6,9 @@ function Navbar() {
   return (
     <div className='nabvarContainer'>
         <div className='leftSide'>
+        <div className='imgg'>
+          <img src="https://images.unsplash.com/photo-1655031682023-5a8964edbfac?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60"/>
+        </div>
         <h2>Stringar</h2>
         </div>
 
